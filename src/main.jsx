4a7a0229be161a548eb6de39bb66e58ed816e149
Mishka Mebonia <mishkas.mebonia@gmail.com>
2023-09-30 23:30:44 +0400

    trying to fix script

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './scss/style.scss'
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './App.jsx'
+// import './scss/style.scss'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+import './scss/style.scss';
+
+ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+  document.getElementById('root')
+);
