839a5a4c0d15780a4a2f7ab0f91b59a02d59f174
Mishka Mebonia <mishkas.mebonia@gmail.com>
2023-09-30 23:49:06 +0400

    trying to fix script

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+// vite.config.js
+import react from '@vitejs/plugin-react';
+
+export default {
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist', // This should match your output directory
+  },
+};
