a5883d5239751d05f2ef6b1e953ced69e92f5320
Mishka Mebonia <mishkas.mebonia@gmail.com>
2023-10-01 00:00:05 +0400

    trying to fix script

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,7 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/credit-card-react/', // Replace 'your-repo-name' with your actual GitHub repository name
   plugins: [react()],
-  build: {
-    outDir: 'docs', // GitHub Pages serves from the 'docs' directory by default
-  },
 })
